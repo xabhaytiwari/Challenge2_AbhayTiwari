0fb232da6fe7c88e33150dd2a2b9a894fa843e5f
Abhay Tiwari <emailatabhay@gmail.com>
2026-03-15 01:39:03 +0530

    Fixed: Device issue on visua_local

--- visual_local.py
+++ visual_local.py
@@ -1,14 +1,22 @@
 from diffusers import DiffusionPipeline
 import torch
 
-# Load model once (important for speed)
-pipe = DiffusionPipeline.from_pretrained(
+device = "cuda" if torch.cuda.is_available() else "cpu"
+if(device != "cuda"):
+    pipe = DiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
     torch_dtype=torch.float32,
     #use_safetensors=True,
     #variant="fp16"
 )
-device = "cuda" if torch.cuda.is_available() else "cpu"
+else:
+    pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16"
+)
+
 pipe.to(device)
 
 
