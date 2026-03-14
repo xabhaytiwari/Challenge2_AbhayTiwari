9619717856b45268561b3fe168550e9a685887ca
Abhay Tiwari <emailatabhay@gmail.com>
2026-03-15 01:40:30 +0530

    Logic Error Fix

--- visual_local.py
+++ visual_local.py
@@ -2,20 +2,19 @@ from diffusers import DiffusionPipeline
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-if(device != "cuda"):
+
+if device == "cuda":
     pipe = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float32,
-    #use_safetensors=True,
-    #variant="fp16"
-)
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16"
+    )
 else:
     pipe = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16"
-)
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        torch_dtype=torch.float32
+    )
 
 pipe.to(device)
 
